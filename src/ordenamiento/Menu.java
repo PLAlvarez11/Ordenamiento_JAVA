e472c9497b0c65398c21d563ee62e013d3e9dfdd
PLAlvarez11 <pluis.alvarezg@gmail.com>
2024-03-23 00:04:46 -0600

    Documentacion

--- src/ordenamiento/Menu.java
+++ src/ordenamiento/Menu.java
@@ -9,7 +9,15 @@ public class Menu {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+		System.out.println("Ingrese números separados por coma");
+        String cadenaNumeros = scanner.nextLine();
+        
+        String[] numerosStr = cadenaNumeros.split(",");
+        int[] arr = new int[numerosStr.length];
+        
+        for (int i = 0; i < numerosStr.length; i++) {
+        	arr[i] = Integer.parseInt(numerosStr[i]);
+        }
 
         int opcion;
         int N;
@@ -20,84 +28,63 @@ public class Menu {
 
             switch (opcion) {
                 case 1:
-                    System.out.println("**Selección sort (Selección)**");
-                                  
-                    sort(arr); 
-                    System.out.println("Sorted array"); 
+                    System.out.println("Selección sort");   
+                    sort(arr);  
                     printSort(arr); 
                     
                     break;
                 case 2:
-                    System.out.println("**Bubble sort (Burbuja)**");
-                                                            
+                    System.out.println("Bubble sort");                              
                     int n = arr.length;
                     bubbleSort(arr, n);
-                    System.out.println("Sorted array: ");
                     printBubble(arr, n);
                     
                     break;
                 case 3:
-                    System.out.println("**Insertion sort (Inserción)**");
-                    
+                    System.out.println("Insertion sort");
                     insertion(arr);
                     printInsertion(arr);
                     
                     break;
                 case 4:
-                    System.out.println("**Merge sort (Combinación)**");
-                    
-                    printMerge(arr);                    
+                    System.out.println("Merge sort");             
                     merge(arr, 0, arr.length - 1);
-                    System.out.println("\nSorted array is");
                     printMerge(arr);
                     
                     break;
                 case 5:
-                    System.out.println("**Quick sort (Rápida)**");
-                    
+                    System.out.println("Quick sort");
                      N = arr.length;
-
-                    // Function call
                     quickSort(arr, 0, N - 1);
-                    System.out.println("Sorted array:");
                     printArrQuick(arr);
                     
                     break;
                 case 6:
-                    System.out.println("**Heap sort (Montón)**");
-                    
+                    System.out.println("Heap sort");
                     N = arr.length;                    
                     heap(arr);
-                    System.out.println("Sorted array is");
                     printHeap(arr);
                     
                     break;
                 case 7:
-                    System.out.println("**Counting sort (Conteo)**");
-                    
+                    System.out.println("Counting sort (Conteo)");
                     int[] outputArray = counting(arr);
-             
                     for (int i = 0; i < arr.length; i++) {
                         System.out.print(outputArray[i] + " ");
                     }
                     
                     break;
                 case 8:
-                    System.out.println("**Radix sort (Raíz)**");
-                    	
+                    System.out.println("Radix sort");
                     N= arr.length;
-                    
-                    // Function Call
                     radix(arr, N);
                     printRadix(arr, N);
                     
                     break;
                 case 9:
-                    System.out.println("**Bucket sort (Cubo)**");
-                    
+                    System.out.println("Bucket sort");
                     float[] arrF = {0.897f, 0.565f, 0.656f, 0.1234f, 0.665f, 0.3434f};
                     bucket(arrF);
-                    System.out.println("Sorted array is:");
                     for (float num : arr) {
                         System.out.print(num + " ");
                     }
@@ -115,18 +102,18 @@ public class Menu {
     }
 
     private static void mostrarMenu() {
-        System.out.println("\n**Como desea ordenar**");
+        System.out.println("\nElija un opcion para ordenar");
         System.out.println("-------------------------------------");
-        System.out.println("1. Selection sort (Selección)");
-        System.out.println("2. Bubble sort (Burbuja)");
-        System.out.println("3. Insertion sort (Inserción)");
-        System.out.println("4. Merge sort (Combinación)");
-        System.out.println("5. Quick sort (Rápida)");
-        System.out.println("6. Heap sort (Montón)");
-        System.out.println("7. Counting sort (Conteo)");
-        System.out.println("8. Radix sort (Raíz)");
-        System.out.println("9. Bucket sort (Cubo)");
-        System.out.println("10. Salir");
+        System.out.println("1 -> Selection sort");
+        System.out.println("2 -> Bubble sort");
+        System.out.println("3 -> Insertion sort");
+        System.out.println("4 -> Merge sort");
+        System.out.println("5 -> Quick sort");
+        System.out.println("6 -> Heap sort");
+        System.out.println("7 -> Counting sort");
+        System.out.println("8 -> Radix sort");
+        System.out.println("9 -> Bucket sort");
+        System.out.println("10-> Salir");
         System.out.println("-------------------------------------");
         System.out.print("Seleccione una opción: ");
     }
@@ -134,18 +121,14 @@ public class Menu {
     public static void sort(int arr[]) 
     { 
         int n = arr.length; 
-  
-        // One by one move boundary of unsorted subarray 
+
         for (int i = 0; i < n-1; i++) 
         { 
-            // Find the minimum element in unsorted array 
             int min_idx = i; 
             for (int j = i+1; j < n; j++) 
                 if (arr[j] < arr[min_idx]) 
                     min_idx = j; 
-  
-            // Swap the found minimum element with the first 
-            // element 
+
             int temp = arr[min_idx]; 
             arr[min_idx] = arr[i]; 
             arr[i] = temp; 
@@ -168,19 +151,16 @@ public class Menu {
             swapped = false;
             for (j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                     
-                    // Swap arr[j] and arr[j+1]
+
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
                 }
             }
- 
-            // If no two elements were
-            // swapped by inner loop, then break
-            if (swapped == false)
-                break;
+
+        if (swapped == false)
+            break;
         }
     }
     
@@ -198,10 +178,7 @@ public class Menu {
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
- 
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
+
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -222,26 +199,20 @@ public class Menu {
     
     public static void merge(int arr[], int l, int m, int r)
     {
-        // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
  
-        // Create temp arrays
         int L[] = new int[n1];
         int R[] = new int[n2];
  
-        // Copy data to temp arrays
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
  
-        // Merge the temp arrays
- 
-        // Initial indices of first and second subarrays
+
         int i = 0, j = 0;
- 
-        // Initial index of merged subarray array
+
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -255,14 +226,12 @@ public class Menu {
             k++;
         }
  
-        // Copy remaining elements of L[] if any
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
  
-        // Copy remaining elements of R[] if any
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -274,14 +243,10 @@ public class Menu {
     {
         if (l < r) {
  
-            // Find the middle point
             int m = l + (r - l) / 2;
  
-            // Sort first and second halves
             merge(arr, l, m);
             merge(arr, m + 1, r);
- 
-            // Merge the sorted halves
             merge(arr, l, m, r);
         }
     }
@@ -303,19 +268,13 @@ public class Menu {
     
     static int partition(int[] arr, int low, int high)
     {
-        // Choosing the pivot
         int pivot = arr[high];
-
-        // Index of smaller element and indicates
-        // the right position of pivot found so far
         int i = (low - 1);
 
         for (int j = low; j <= high - 1; j++) {
 
-            // If current element is smaller than the pivot
             if (arr[j] < pivot) {
 
-                // Increment index of smaller element
                 i++;
                 swap(arr, i, j);
             }
@@ -328,12 +287,8 @@ public class Menu {
     {
         if (low < high) {
 
-            // pi is partitioning index, arr[p]
-            // is now at right place
             int pi = partition(arr, low, high);
 
-            // Separately sort elements before
-            // partition and after partition
             quickSort(arr, low, pi - 1);
             quickSort(arr, pi + 1, high);
         }
@@ -350,43 +305,34 @@ public class Menu {
     {
         int N = arr.length;
  
-        // Build heap (rearrange array)
         for (int i = N / 2 - 1; i >= 0; i--)
             heapify(arr, N, i);
- 
-        // One by one extract an element from heap
+
         for (int i = N - 1; i > 0; i--) {
-            // Move current root to end
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
- 
-            // call max heapify on the reduced heap
             heapify(arr, i, 0);
         }
     }
     
     public static void heapify(int arr[], int N, int i)
     {
-        int largest = i; // Initialize largest as root
-        int l = 2 * i + 1; // left = 2*i + 1
-        int r = 2 * i + 2; // right = 2*i + 2
+        int largest = i;
+        int l = 2 * i + 1; 
+        int r = 2 * i + 2;
  
-        // If left child is larger than root
         if (l < N && arr[l] > arr[largest])
             largest = l;
- 
-        // If right child is larger than largest so far
+
         if (r < N && arr[r] > arr[largest])
             largest = r;
  
-        // If largest is not root
         if (largest != i) {
             int swap = arr[i];
             arr[i] = arr[largest];
             arr[largest] = swap;
  
-            // Recursively heapify the affected sub-tree
             heapify(arr, N, largest);
         }
     }
@@ -430,12 +376,8 @@ public class Menu {
     
     public static void radix(int arr[], int n)
     {
-        // Find the maximum number to know number of digits
         int m = getMax(arr, n);
- 
-        // Do counting sort for every digit. Note that
-        // instead of passing digit number, exp is passed.
-        // exp is 10^i where i is current digit number
+
         for (int exp = 1; m / exp > 0; exp *= 10)
             countSort(arr, n, exp);
     }
@@ -448,29 +390,22 @@ public class Menu {
     
     static void countSort(int arr[], int n, int exp)
     {
-        int output[] = new int[n]; // output array
+        int output[] = new int[n]; 
         int i;
         int count[] = new int[10];
         Arrays.fill(count, 0);
  
-        // Store count of occurrences in count[]
         for (i = 0; i < n; i++)
             count[(arr[i] / exp) % 10]++;
  
-        // Change count[i] so that count[i] now contains
-        // actual position of this digit in output[]
         for (i = 1; i < 10; i++)
             count[i] += count[i - 1];
  
-        // Build the output array
         for (i = n - 1; i >= 0; i--) {
             output[count[(arr[i] / exp) % 10] - 1] = arr[i];
             count[(arr[i] / exp) % 10]--;
         }
  
-        // Copy the output array to arr[], so that arr[] now
-        // contains sorted numbers according to current
-        // digit
         for (i = 0; i < n; i++)
             arr[i] = output[i];
     }
@@ -487,24 +422,20 @@ public class Menu {
     public static void bucket(float[] arr) {
         int n = arr.length;
 
-        // 1) Create n empty buckets
         List<Float>[] buckets = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             buckets[i] = new ArrayList<>();
         }
 
-        // 2) Put array elements in different buckets
         for (int i = 0; i < n; i++) {
             int bi = (int) (n * arr[i]);
             buckets[bi].add(arr[i]);
         }
 
-        // 3) Sort individual buckets using insertion sort
         for (int i = 0; i < n; i++) {
             insertionSort(buckets[i]);
         }
 
-        // 4) Concatenate all buckets into arr[]
         int index = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < buckets[i].size(); j++) {
